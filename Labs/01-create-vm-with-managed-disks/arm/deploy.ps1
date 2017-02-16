fb307c5b55ff931e3c1a56105b516ff539d99a6b
Senthuran Sivananthan [MSFT] <sesivan@microsoft.com>
2017-02-16 07:59:13 -0500

    Documented PS scripts

--- Labs/01-create-vm-with-managed-disks/arm/deploy.ps1
+++ Labs/01-create-vm-with-managed-disks/arm/deploy.ps1
@@ -1,10 +1,13 @@
-﻿Select-AzureRmSubscription -SubscriptionName "Microsoft Azure Internal Consumption"
-
-$ResourceGroupName = "demo-managed-disks-1"
+﻿$ResourceGroupName = "demo-md-1"
 $Location = "canadacentral"
 
+# Select Subscription
+Select-AzureRmSubscription -SubscriptionName "Microsoft Azure Internal Consumption"
+
+# Create new resource group
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location -Verbose
 
+# Deploy ARM Template
 New-AzureRmResourceGroupDeployment `
     -Name "deploy" `
     -ResourceGroupName $ResourceGroupName `
--- Labs/04-migrate-unmanaged-disks/01-build-unmanaged-vms/deploy.ps1
+++ Labs/04-migrate-unmanaged-disks/01-build-unmanaged-vms/deploy.ps1
@@ -1,10 +1,13 @@
-﻿Select-AzureRmSubscription -SubscriptionName "Microsoft Azure Internal Consumption"
+﻿$ResourceGroupName = "demo-md-1"
+$Location = "canadacentral"
 
-$ResourceGroupName = "demo-md-a"
-$Location = "eastus2"
+# Select Subscription
+Select-AzureRmSubscription -SubscriptionName "Microsoft Azure Internal Consumption"
 
+# Create new resource group
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location -Verbose
 
+# Deploy ARM Template
 New-AzureRmResourceGroupDeployment `
     -Name "deploy" `
     -ResourceGroupName $ResourceGroupName `

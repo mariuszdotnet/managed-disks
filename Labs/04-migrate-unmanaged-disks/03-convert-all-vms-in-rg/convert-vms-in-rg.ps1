10b2772393e487380070bcdadaf98309286512c4
Senthuran Sivananthan <sesivan@microsoft.com>
2017-03-21 14:23:47 -0400

    Added script to convert all VMs in Resource Group

---
+++ Labs/04-migrate-unmanaged-disks/03-convert-all-vms-in-rg/convert-vms-in-rg.ps1
@@ -0,0 +1,19 @@
+﻿$ResourceGroupName = 'test-2'
+
+# Select Subscription
+Select-AzureRmSubscription -SubscriptionName "Lab" -Verbose
+
+$VMs = Get-AzureRmVM -ResourceGroupName $ResourceGroupName
+
+# Convert all of the VMs
+foreach($VM in $VMs)
+{
+    # Stop VM
+    Stop-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $VM.Name -Force -Verbose
+
+    # Convert
+    ConvertTo-AzureRmVMManagedDisk -ResourceGroupName $ResourceGroupName -VMName $VM.Name -Verbose
+
+    # Start VM
+    Start-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $VM.Name
+}
